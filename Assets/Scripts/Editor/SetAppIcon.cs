5c7ee166650a8c0ae0b536682d18bac7ca5e0ca2
Samil <samil@example.com>
2026-06-08 17:37:36 +0300

    Fix SetAppIcon compile error

--- Assets/Scripts/Editor/SetAppIcon.cs
+++ Assets/Scripts/Editor/SetAppIcon.cs
@@ -1,6 +1,5 @@
 using UnityEditor;
 using UnityEngine;
-using UnityEditor.Build;
 
 [InitializeOnLoad]
 public class SetAppIcon
@@ -15,19 +14,18 @@ public class SetAppIcon
         Texture2D icon = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Textures/AppIcon.png");
         if (icon != null)
         {
-            var iosTarget = NamedBuildTarget.iOS;
-            var iOSIconKinds = PlayerSettings.GetSupportedIconKinds(iosTarget);
-            foreach (var kind in iOSIconKinds)
-            {
-                var sizes = PlayerSettings.GetIconSizes(iosTarget, kind);
-                Texture2D[] kindIcons = new Texture2D[sizes.Length];
-                for (int i = 0; i < sizes.Length; i++)
-                {
-                    kindIcons[i] = icon;
-                }
-                PlayerSettings.SetIcons(iosTarget, kindIcons, kind);
-            }
+#pragma warning disable 0618
+            PlayerSettings.SetIconsForTargetGroup(BuildTargetGroup.Unknown, new Texture2D[] { icon });
             
+            // To be extra safe, set it specifically for iOS too, letting Unity scale it
+            int[] sizes = PlayerSettings.GetIconSizesForTargetGroup(BuildTargetGroup.iOS);
+            Texture2D[] icons = new Texture2D[sizes.Length];
+            for (int i = 0; i < sizes.Length; i++) {
+                icons[i] = icon;
+            }
+            PlayerSettings.SetIconsForTargetGroup(BuildTargetGroup.iOS, icons);
+#pragma warning restore 0618
+
             AssetDatabase.SaveAssets();
         }
     }
